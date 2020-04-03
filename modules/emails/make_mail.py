e82d24de6fe4875c4934eca976103154b196d52c
Sara <sara.rufael@gmail.com>
2020-04-02 23:31:28 -0500

    reformmatted remaining .py files

--- modules/emails/make_mail.py
+++ modules/emails/make_mail.py
@@ -5,25 +5,25 @@ from datetime import datetime
 from datetime import timedelta
 import random
 import nltk
-import json 
+import json
 import hashlib
 
-    
 date_time = datetime(1988, 6, 29, 8, 00, 00)
+
 
 class Email:
 
-    def __init__(self, date_time, sender_domains, sender, 
-                recipient, corpus, result=None, subject=None, link=None,
-                reply_to=None):
+    def __init__(self, date_time, sender_domains, sender,
+                 recipient, corpus, result=None, subject=None, link=None,
+                 reply_to=None):
 
         self.time = date_time.strftime("%a %b %d %H:%M:%S %Y")
         self.body = generate_text(corpus, 20)
         self.sender = sender
         self.recipient = recipient
         self.hash = self.get_hash()
-        
-        self.filename = filename =  "email_%s" % self.hash.hexdigest() 
+
+        self.filename = filename = "email_%s" % self.hash.hexdigest()
         self.link = link
         if not result:
             self.result = random.choice(["Accepted", "Blocked"])
@@ -44,33 +44,33 @@ class Email:
             self.reply_to = sender
         else:
             self.reply_to = reply_to
-        
+
     def stringify(self):
         """return json object with email attributes"""
         return {
-                    "event_time": self.time,
-                    "sender": self.sender,
-                    "reply_to": self.reply_to,
-                    "recipient": self.recipient,
-                    "filename": self.filename,
-                    "subject": self.subject,
-                    "result": self.result,
-                    "link":self.link
-                }
+            "event_time": self.time,
+            "sender": self.sender,
+            "reply_to": self.reply_to,
+            "recipient": self.recipient,
+            "filename": self.filename,
+            "subject": self.subject,
+            "result": self.result,
+            "link": self.link
+        }
 
     def get_hash(self):
         val = str(self.time) + self.sender
         hash = hashlib.sha1(str(val).encode('utf-8'))
         return hash
 
-    
+
 def generate_text(corpus, length):
     """
     Use nlkt to generate a paragraph of a given length
     given some feeder text
     """
 
-    words = [x.strip() for x in corpus.split(' ')] 
+    words = [x.strip() for x in corpus.split(' ')]
     # NLTK shortcuts :)
     bigrams = [b for b in zip(words[:-1], words[1:])]
     cfd = nltk.ConditionalFreqDist(bigrams)
@@ -78,7 +78,7 @@ def generate_text(corpus, length):
     # pick a random word from the corpus to start with
     word = random.choice(words)
     # generate 15 more words
-    
+
     final_text = ""
     for i in range(length):
         final_text = final_text + word + " "
@@ -95,17 +95,17 @@ def gen_link(corpus):
     # thanks to: https://www.geeksforgeeks.org/python-remove-all-characters-except-letters-and-numbers/
     import re
     pre = random.choice(["http://",
-                        "https://",
-                        "www.", 
-                        "http://wwww.",
-                        "https://www.", ""])
+                         "https://",
+                         "www.",
+                         "http://wwww.",
+                         "https://www.", ""])
     tld = random.choice([".com", ".org", ".net", ".biz", ".co", ".co.uk"])
     ini_string = generate_text(corpus, 3)
     # remove non-alphanumeracal characters
     domain = re.sub('[\W_]+', '', ini_string).lower()
     link = pre + domain + tld
     return link
-    
+
 
 def create_email_obj(email, corpus, template_obj, output_path):
     """
@@ -114,14 +114,12 @@ def create_email_obj(email, corpus, template_obj, output_path):
     Corpus is the body of text used to generate the emails
     """
     t = template_obj
-    content =  t.render(body = generate_text(corpus, 100),
-                        sender = email['sender'],
-                        reply_to = email['reply_to'],
-                        time = email['event_time'],
-                        recipient = email['recipient'],
-                        subject = email['subject'],
-                        link= email['link'])        
+    content = t.render(body=generate_text(corpus, 100),
+                       sender=email['sender'],
+                       reply_to=email['reply_to'],
+                       time=email['event_time'],
+                       recipient=email['recipient'],
+                       subject=email['subject'],
+                       link=email['link'])
 
     return content
-
-
--- modules/outbound_browsing/make_outbound_traffic.py
+++ modules/outbound_browsing/make_outbound_traffic.py
@@ -1,11 +1,12 @@
-import random 
+import random
 import string
 
 METHODS = ["GET"]
 STATUS_CODES = ["202", "301", "302", "404", "403"]
 
+
 class OutboundEvent:
-        
+
     def __init__(self, time, hosts, endpoints, user=None, endpoint=None, request=None):
         """Set initial values"""
         self.time = time
@@ -25,30 +26,28 @@ class OutboundEvent:
             self.request = request
         else:
             self.set_request()
-            
+
         self.src_ip = self.current_user["ip_addr"]
         self.user_agent = self.current_user["user_agent"]
         self.url = self.current_endpoint.split("/")[0]
-        self.dst_ip  = self.current_endpoint.split("/")[1].strip()
+        self.dst_ip = self.current_endpoint.split("/")[1].strip()
         self.set_method()
         self.set_status_code()
 
-        
-    
     def set_new_user(self):
         """
         If no specific user was specified
         Choose a random user for outbound browsing event
         """
         self.current_user = random.choice(self.hosts)
-        
+
     def set_new_endpoint(self):
         """
         External website user is browsing to
-        
+
         """
         self.current_endpoint = random.choice(self.endpoints)
-        
+
     def set_method(self):
         """
         Request method
@@ -56,26 +55,28 @@ class OutboundEvent:
         Currently limited to GET ONLY
         """
         self.method = random.choice(METHODS)
-    
+
     def set_status_code(self):
         """
         Result of the request
         Chosen randomly from status codes defined above
         """
         self.status_code = random.choice(STATUS_CODES)
-    
+
     def stringify(self):
         """Print the data in one convinient line"""
-        logline = ' '.join([str(x) for x in [self.time, self.method, self.src_ip, self.url, self.request, self.dst_ip, self.user_agent]])
+        logline = ' '.join([str(x) for x in [self.time, self.method, self.src_ip, self.url, self.request, self.dst_ip,
+                                             self.user_agent]])
 
         return logline
 
-
     def set_request(self, stringLength=10):
         """
-        Generate a random string of fixed length 
+        Generate a random string of fixed length
         thanks to: https://pynative.com/python-generate-random-string/
         """
         letters = string.ascii_lowercase
-        extension = random.choice(['.html', '.js', '.css', '.png', '.jpg', '.exe', '.docx', '.xls', ''])
-        self.request = ''.join(random.choice(letters) for i in range(stringLength)) + extension
+        extension = random.choice(['.html', '.js', '.css', '.png',
+                                   '.jpg', '.exe', '.docx', '.xls', ''])
+        self.request = ''.join(
+            random.choice(letters) for i in range(stringLength)) + extension
