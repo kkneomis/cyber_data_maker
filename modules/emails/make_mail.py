3e0eefc8a4c75291f60a14023758f3f5d166c59c
Simeon Kakpovi <skakpovi@gmail.com>
2020-04-18 13:21:45 -0400

    Emails now generated at the same time as mail log Emails now based on config file instead of corpus Changes here by ajhenley

--- modules/emails/make_mail.py
+++ modules/emails/make_mail.py
@@ -2,44 +2,35 @@ import sys
 import os
 
 from datetime import datetime
+from dateutil.parser import parse
 from datetime import timedelta
 import random
-import nltk
 import json
 import hashlib
 
-from modules.emails.generate_email import generateEmail
-
 date_time = datetime(1988, 6, 29, 8, 00, 00)
-
 
 class Email:
 
-    def __init__(self, date_time, sender_domains, sender,
-                 recipient, corpus, result=None, subject=None, link=None,
-                 reply_to=None):
+    def __init__(self, date_time, sender, recipient, subject, body, result=None, link=None, reply_to=None):
 
         self.time = date_time.strftime("%a %b %d %H:%M:%S %Y")
-        self.body = generateEmail()
+        self.subject = subject
+        self.body = body
         self.sender = sender
         self.recipient = recipient
         self.hash = self.get_hash()
 
         self.filename = filename = "email_%s" % self.hash.hexdigest()
         self.link = link
+        
         if not result:
             self.result = random.choice(["Accepted", "Blocked"])
         else:
             self.result = result
 
-        if not subject:
-            text = self.body.split()
-            self.subject = text[:5]
-        else:
-            self.subject = subject
-
         if not link:
-            self.link = gen_link(corpus)
+            self.link = gen_link()
         else:
             self.link = link
 
@@ -47,6 +38,7 @@ class Email:
             self.reply_to = sender
         else:
             self.reply_to = reply_to
+            
 
     def stringify(self):
         """return json object with email attributes"""
@@ -57,8 +49,21 @@ class Email:
             "recipient": self.recipient,
             "filename": self.filename,
             "subject": self.subject,
+            "body": self.body,
             "result": self.result,
             "link": self.link
+        }
+    
+    def stringifysimple(self):
+        """return json object with email attributes"""
+        return {
+            "event_time": self.time,
+            "sender": self.sender,
+            "reply_to": self.reply_to,
+            "recipient": self.recipient,
+            "filename": self.filename,
+            "subject": self.subject,
+            "result": self.result
         }
 
     def get_hash(self):
@@ -67,33 +72,7 @@ class Email:
         return hash
 
 
-def generate_text(corpus, length):
-    """
-    Use nlkt to generate a paragraph of a given length
-    given some feeder text
-    """
-
-    words = [x.strip() for x in corpus.split(' ')]
-    # NLTK shortcuts :)
-    bigrams = [b for b in zip(words[:-1], words[1:])]
-    cfd = nltk.ConditionalFreqDist(bigrams)
-
-    # pick a random word from the corpus to start with
-    word = random.choice(words)
-    # generate 15 more words
-
-    final_text = ""
-    for i in range(length):
-        final_text = final_text + word + " "
-        if word in cfd:
-            word = random.choice(list(cfd[word].keys()))
-        else:
-            break
-
-    return final_text
-
-
-def gen_link(corpus):
+def gen_link():
     """Generate random strings of three words for dummy url"""
     # thanks to: https://www.geeksforgeeks.org/python-remove-all-characters-except-letters-and-numbers/
     import re
@@ -103,26 +82,19 @@ def gen_link(corpus):
                          "http://wwww.",
                          "https://www.", ""])
     tld = random.choice([".com", ".org", ".net", ".biz", ".co", ".co.uk"])
-    ini_string = generate_text(corpus, 3)
+    ini_string = generate_text("time person year way day thing man world life hand part child eye woman place work week case point government company number group problem fact good new first last long great little own other old right big high different small large next early young important few public bad same able", 3)
     # remove non-alphanumeracal characters
     domain = re.sub('[\W_]+', '', ini_string).lower()
     link = pre + domain + tld
     return link
 
+def generate_text(corpus, length):
+    words = [x.strip() for x in corpus.split(' ')]
+    final_text = ""
+    word = random.choice(words)
+    for i in range(length):
+        final_text = final_text + word + " "
+        word = random.choice(words)
+    return final_text
 
-def create_email_obj(email, corpus, template_obj, output_path):
-    """
-    Given a json email object
-    create generate an email message
-    Corpus is the body of text used to generate the emails
-    """
-    t = template_obj
-    content = t.render(body=generate_text(corpus, 100),
-                       sender=email['sender'],
-                       reply_to=email['reply_to'],
-                       time=email['event_time'],
-                       recipient=email['recipient'],
-                       subject=email['subject'],
-                       link=email['link'])
 
-    return content
